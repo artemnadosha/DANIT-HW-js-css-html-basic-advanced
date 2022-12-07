1c4d39d8d1ec198fbcb20f87098b7fe0fb7d6966
artemnadosha <artemnadosha@gmail.com>
2022-12-08 01:05:26 +0200

    homework11

--- homework11/index.js
+++ homework11/index.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('.password-form');
 const inputLabel = form.querySelectorAll('.input-wrapper');
 const icon = form.querySelectorAll('.icon-password');
-const validList = document.querySelectorAll('.list-valid');
+const validList = document.querySelector('.list-valid');
 const firstInput = form.querySelector('input');
 const buttonSubmit = form.querySelector('button');
 
@@ -47,53 +47,40 @@ form.addEventListener('submit', e => {
 buttonSubmit.disabled = true;
 
 firstInput.addEventListener('keyup', e => {
-    const allRegular = /[0-9]*[A-Z]*[А-Я]*[а-я]*[a-z]/g;
 
-   if(/\d/g.test(e.target.value)) {
-       validList.forEach(e => {
-           e.querySelector('[data-text=number]').style.color = 'green';
-       });
+    const checkLitterUp = /[A-Z]/g.test(e.target.value);
+    const checkLitterDown = /[a-z]/g.test(e.target.value);
+    const ckeckLength = e.target.value.length >= 8;
+    const checkNumber = /[0-9]/g.test(e.target.value)
+
+   if(checkNumber) {
+       validList.querySelector('[data-text=number]').style.color = 'green';
    } else {
-       validList.forEach(e => {
-           e.querySelector('[data-text=number]').style.color = 'red';
-       });
+       validList.querySelector('[data-text=number]').style.color = 'red';
    };
 
-    if(/[A-Z]|[А-Я]/g.test(e.target.value)) {
-        validList.forEach(e => {
-            e.querySelector('[data-text=big]').style.color = 'green';
-        });
+    if(checkLitterUp) {
+        validList.querySelector('[data-text=big]').style.color = 'green';
     } else {
-        validList.forEach(e => {
-            e.querySelector('[data-text=big]').style.color = 'red';
-        });
+        validList.querySelector('[data-text=big]').style.color = 'red';
     };
 
-    if(/[a-z]|[а-я]/g.test(e.target.value)) {
-        validList.forEach(e => {
-            e.querySelector('[data-text=small]').style.color = 'green';
-        });
+    if(checkLitterDown) {
+        validList.querySelector('[data-text=small]').style.color = 'green';
     } else {
-        validList.forEach(e => {
-            e.querySelector('[data-text=small]').style.color = 'red';
-        });
+        validList.querySelector('[data-text=small]').style.color = 'red';
     };
 
-    if(e.target.value.length >= 8) {
-        validList.forEach(e => {
-            e.querySelector('[data-text=length]').style.color = 'green';
-        });
+    if(ckeckLength) {
+        validList.querySelector('[data-text=length]').style.color = 'green';
     } else {
-        validList.forEach(e => {
-            e.querySelector('[data-text=length]').style.color = 'red';
-        });
+        validList.querySelector('[data-text=length]').style.color = 'red';
     };
 
-    if(allRegular.test(e.target.value) && e.target.value.length >= 8){
-        buttonSubmit.disabled = false;
-    } else {
-        buttonSubmit.disabled = true;
-    };
+    if(checkLitterUp && checkLitterDown && ckeckLength && checkNumber) {
+            buttonSubmit.disabled = false;
+        } else {
+            buttonSubmit.disabled = true;
+        };
 });
-
 
