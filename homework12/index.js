f012ca6ba5620ea80707cf3f4a89732e1aaff7c2
artemnadosha <artemnadosha@gmail.com>
2022-12-08 15:13:35 +0200

    homework12

--- homework12/index.js
+++ homework12/index.js
@@ -2,11 +2,11 @@ const buttonWrapper = document.querySelector('.btn-wrapper')
 const allButton = buttonWrapper.querySelectorAll('button');
 document.body.addEventListener('keydown', e => {
     allButton.forEach(el => {
-        el.style.backgroundColor = 'black';
-
-        if(e.key.toUpperCase() === el.textContent || e.key === el.textContent) {
-            el.style.backgroundColor = 'blue';
-        };
+            if(e.key.toUpperCase() === el.textContent.toUpperCase()) {
+                el.style.backgroundColor = 'blue';
+            } else {
+                el.style.backgroundColor = 'black';
+            };
     });
 });
 
